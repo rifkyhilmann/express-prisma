57e1406234c877c7ef8fda1998feacabfc39ccfc
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2024-10-26 00:31:07 +0700

    menambah fitur login

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,11 @@ app.get('/', (req : Request, res : Response) => {
     res.send("Hello World!");
 })
 
+app.get('/hello/world', (req : Request, res : Response) => {
+    res.send("Hello World!");
+})
+
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
+
